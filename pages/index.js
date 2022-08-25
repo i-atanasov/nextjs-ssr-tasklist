76044b299a40d98077785b093664c0699fe56d34
i-atanasov <ivayloatanasov@zohomail.eu>
2022-08-25 17:16:21 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 //import styles from '../styles/Home.module.css'
-import Link from 'next/link';
-import TasksList from './tasksList';
+//import Link from 'next/link';
+import TasksList from '../components/TaskList';
 
 const Home = () => {
   return (
